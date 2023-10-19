2eeb0b741eff3cc206a8aa991d638d1a2fd0227c
gmth <143313748+gmth6789@users.noreply.github.com>
2023-10-19 13:17:21 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,10 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: 'LIFF App',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
