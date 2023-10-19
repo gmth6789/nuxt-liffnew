089b7f5824cc8077bc47c138a3c74bb75ff0b9c7
gmth <143313748+gmth6789@users.noreply.github.com>
2023-10-19 12:20:30 +0000

    Update home.vue

--- pages/home.vue
+++ pages/home.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Done</div>
+  <div>
+    <v-app-bar
+      color="primary"
+      dense
+      flat
+      dark
+    >
+      <v-toolbar-title>Home</v-toolbar-title>
+    </v-app-bar>
+  <div class="home">
+  
+    <div class="home__buttons">
+     <NuxtLink to="/reward">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        class="home__buttons__button button--secondary"
+      >
+       ไปที่หน้าแชร์
+      </a></NuxtLink>
+    </div></div>
+  </div>
 </template>
